0cb1d296fce34aba3b9e8bd6bcbb79120c97c186
momf <mail@ni-vg.com>
2020-02-01 18:05:35 +0900

    Fixed post (#2)
    * update
    
    * remove fuckin tokio
    
    * remove tab
    
    * rename GsltServerData to GsltData
    
    * add url crate, and impl fn encode
    
    * fixed type of arg
    
    * fixed body
    
    * revert POST

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::*;
 use serde::{Deserialize, Serialize};
+use url::form_urlencoded;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct GsltCredentialResponse {
@@ -40,12 +41,16 @@ struct GsltData  {
 fn main() -> Result<()> {
     let list = get_list("STEAM_WEB_API_KEY")?;
     println!("list: {:?}", list);
+
+    let gslt = create_gslt("STEAM_WEB_API_KEY", 730, "MEMO")?;
+    println!("gslt: {:?}", gslt);
+
     Ok(())
 }
 
 fn get_list(steam_web_api_key: impl Into<String>) -> Result<GsltListResponse> {
     let steam_web_api_key = steam_web_api_key.into();
-    let request_url = format!("https://api.steampowered.com/IGameServersService/GetAccountList/v1/?key={}", steam_web_api_key);
+    let request_url = format!("https://api.steampowered.com/IGameServersService/GetAccountList/v1/?key={}", encode(&steam_web_api_key));
 
     let response = ureq::get(&request_url)
                         .timeout_connect(10_000)
@@ -60,21 +65,26 @@ fn get_list(steam_web_api_key: impl Into<String>) -> Result<GsltListResponse> {
     Ok(list)
 }
 
-fn get_gslt(steam_web_api_key: impl Into<String>, app_id: impl Into<String>, memo: impl Into<String>) -> Result<GsltCredentialResponse> {
+fn create_gslt(steam_web_api_key: impl Into<String>, app_id: u32, memo: impl Into<String>) -> Result<GsltCredentialResponse> {
     let steam_web_api_key = steam_web_api_key.into();
-    let app_id = app_id.into();
+    let app_id = app_id.to_string();
     let memo = memo.into();
-    let request_url = format!("https://api.steampowered.com/IGameServersService/CreateAccount/v1/?key={}&appid={}&memo={}", steam_web_api_key, app_id, memo);
+    let request_url = format!("https://api.steampowered.com/IGameServersService/CreateAccount/v1/?key={}&appid={}&memo={}", encode(&steam_web_api_key), encode(&app_id), encode(&memo));
 
     let response = ureq::post(&request_url)
                         .timeout_connect(10_000)
+                        .set("Content-Length", "0")
                         .call();
 
     if response.status() != 200 { anyhow!("status code is not 200."); }
 
     let body = response.into_string()?;
 
-    let list: GsltCredentialResponse = serde_json::from_str(&body).with_context(|| format!("Failed parse json."))?;
+    let gslt: GsltCredentialResponse = serde_json::from_str(&body).with_context(|| format!("Failed parse json."))?;
 
-    Ok(list)
+    Ok(gslt)
+}
+
+fn encode(s: &str) -> String {
+    form_urlencoded::byte_serialize(s.as_bytes()).collect::<String>()
 }
